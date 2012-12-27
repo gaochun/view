380146e1c0ad9a15a08ecf491507aff496f41c7b
Gao Chun <chun.gao@intel.com>
2012-12-27 15:22:09 +0800

    First commit.

---
+++ projects/ieview/gen/global/COM_config.h
@@ -0,0 +1,26 @@
+/**********************************************************\
+
+  Auto-generated COM_config.h
+
+\**********************************************************/
+
+#define FBTYPELIB_NAME ieviewLib
+#define FBTYPELIB_DESC "ieview 1.0 Type Library"
+#define FBTYPELIB_GUID 23e72741-ac5c-5c21-967c-a2975489db58
+
+#define IFBControl_GUID f639182d-beb0-59fd-bcf0-624649dc46eb
+#define IFBControl_DESC "ieview Control Interface"
+
+#define FBControl0_GUID a343eda9-7353-53dc-a1f4-8fdc4df98d48
+#define ACTIVEX_PROGID0 "GaoChun.ieview"
+#define FBControl_GUID FBControl0_GUID
+#define ACTIVEX_PROGID ACTIVEX_PROGID0
+#define FBControl_DESC "ieview Control Class"
+#define IFBComJavascriptObject_GUID de6995e6-2397-5118-a65e-c339501df7bd
+#define IFBComJavascriptObject_DESC "ieview IComJavascriptObject Interface"
+#define FBComJavascriptObject_GUID 60bf6761-5e2b-51bb-b083-f499cac4a829
+#define FBComJavascriptObject_DESC "ieview ComJavascriptObject Class"
+#define IFBComEventSource_GUID c1bcfab7-f984-5049-937e-19e8641bbe79
+#define IFBComEventSource_DESC "ieview IFBComEventSource Interface"
+#define MOZILLA_PLUGINID "gaochun.com/ieview"
+#define VERSION_NUM "1"
